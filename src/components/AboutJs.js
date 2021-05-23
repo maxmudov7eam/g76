fd16f6113639e1f007f89ac3c641a8b118f60c64
maxmudov7eam <maxmudovdavronbek2000@gmail.com>
2021-05-23 10:54:55 +0300

    practice

---
+++ src/components/AboutJs.js
@@ -0,0 +1,20 @@
+const AboutJs = () => {
+
+    // console.log("abrakadabra".split("a").length - 1);
+
+    function homework(a, b) {
+        console.log(a.split('a').length - 1);
+        console.log(b.split('m').length - 1);
+
+        console.log(a.toLowerCase().split('a'));
+    }
+
+    homework('NimaAsd nimadir', 'lorem')
+
+    return (
+        <>
+            <h5>Lorem, ipsum dolor.</h5>
+        </>
+    )
+}
+export default AboutJs;
